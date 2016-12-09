397fb9cfcbcf79d0c003fa8e69d31a1503a34967
gsanhueza <gabriel_8032@hotmail.com>
2016-12-09 15:40:01 -0300

    en SuffixTree esta la implementacion básica (que seguramente está mala xD)

--- SuffixTreesJava/src/main/SuffixTree.java
+++ SuffixTreesJava/src/main/SuffixTree.java
@@ -1,29 +1,47 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SuffixTree {
-	private String text;
 	private Node root;
 
-	public SuffixTree(String receivedText) {
-		this.text = receivedText;
+	public SuffixTree() {
 		this.root = new Node();
 	}
 
-	public SuffixTree createSuffixTree() {
+	public SuffixTree createSuffixTree(String text) {
 		for (int i = 1; i <= text.length() - 1; i++) {
-			// Fase i
+			// Phase i
 			for (int j = 1; j <= i + 1; j++) {
-				// Extensión j
-				System.out.println("Encontrar el final del camino desde raíz de I, correspondiente a S[" + j + "..." + i + "]");
-				System.out.println("Nos aseguramos de que S[" + j + ".." + "i+1] está en el árbol");
-				System.out.println("Si es necesario, extender ese camino agregando S[i + 1]");
+				// Extension j
+				
+				// Encontrar el final del camino desde raíz de I, correspondiente a S[j...i]
+				Node foundChild = null;
+				for (Node child : root) {
+					if (text.substring(j, i + 1).contains(child.getData())) {
+						foundChild = child;
+						break;
+					}
+				}
+				
+				// Si es necesario, extender ese camino agregando S[i + 1]
+				if (foundChild == null || !foundChild.getData().contains(text.substring(j, i + 2))) {
+					root.addChildren(new Node().setData(text.substring(i + 1, i + 2)));
+				}
 			}
 		}
 		return this;
 	}
 
-	public String[] search(String word) {
-		String[] ans = {"a", "b", "c"};
+	public List<String> search(String word) {
+		ArrayList<String> ans = new ArrayList<String>();
+		
+		for (Node child : root) {
+			if (child.getData().contains(word)) {
+				ans.add(child.getData());
+			}
+		}
 		// TODO Retornar lo que corresponde a búsqueda
 		return ans;
 	}
--- SuffixTreesJava/src/main/TextPreprocessor.java
+++ SuffixTreesJava/src/main/TextPreprocessor.java
@@ -3,14 +3,6 @@ package main;
 public class TextPreprocessor {
 
 	public static String process(String text) {
-		/*
-		return text.toLowerCase()
-				.replace("\n", " ")
-				.replace(".", " ")
-				.replace(":", " ")
-				.replace(",", " ")
-				.replace(";", " ");
-				*/
 		return text.replaceAll("[^a-zA-Z ]", "").toLowerCase();
 	}
 
